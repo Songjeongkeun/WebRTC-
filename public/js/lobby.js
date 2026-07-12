6ce636d6ecc69bcce9fe41ea57a441a0948335ad
이희승 <heseung0895@naver.com>
2026-07-13 01:58:24 +0900

    lobby.js 2차 커밋(roomId 추가, 불필요 삭제, 살짝 다듬)

--- public/js/lobby.js
+++ public/js/lobby.js
@@ -1,17 +1,13 @@
-import express from "express"
-
-const app = express()
-
-const lobbyList = document.querySelector('.lobby-list')
+const lobbyList = document.querySelector('.lobby-list') // 로비 긁어오기
 const createRoomBtn = document.getElementById('create-room')
-const RoomBtn = document.getElementById("lobby-item")
 
-
-// 로비 긁어오기
+// 로비 긁어온 걸 덮어쓰기
 function renderLobby(lobbylist) {
+     // roodId 필요할 것 같아서 추가함 -> roomId로 방을 찾아서 들어가기 때문에 
+     // js에서 data-로 시작하는 숨겨진 속성들만 뽑아옴 아래 lobbyList.addEventListener 참고
     lobbyList.innerHTML = lobbylist.map(room =>
         `
-        <li class="lobby-item">
+        <li class="lobby-item" data-room-id=${room.roomId}>    
             <div class="lobby-item-title">
                 <span>${room.title}</span>
                 <span>${room.current} / 2</span>  
@@ -30,7 +26,7 @@ async function currentLobby() {
         renderLobby(lobbyData)
     }
     catch(error){
-        alert("방 목록을 가져올 수 없음", error)
+        alert("방 목록을 가져올 수 없음")
         console.error("방 목록 가져올 수 없음", error)
     }
 }
@@ -38,18 +34,19 @@ async function currentLobby() {
 // create-room 버튼을 누르면 실행
 createRoomBtn.addEventListener("click", async() => {
     try{
-        const response = await fetch("", {
+        const roomtitle = "방 이름 정해야 됨" // 걍 이름 없는 거 input태그로 가져와야 될 듯. 아님 딴 방법 생각
+
+        const response = await fetch("", {  // 주소 꼭 넣기  중요
             method: "POST", // 겠지??
             headers: { "Content-Type" : "application/json"},
-            body: JSON.stringify({roomtitle})
+            body: JSON.stringify({ roomtitle })
         })
         const data = await response.json()
 
         // 새로 만든 룸으로 이동
-
-
-        
-
+        window.location.href = `/room?roomId=${data.roomId}` 
+        // window(브라우저 창에) location(주소창(주소입력칸)에) href(주소를 입력해라) 여기까지는 읽기만
+        // = 대입 연산자를 기준으로 /room?roomId=${data.roomId} 이 주소로 갈아타라(네트워크 요청 또는 주소로 이동)
     }
     catch(error){
         alert("방 생성 실패")
@@ -59,14 +56,16 @@ createRoomBtn.addEventListener("click", async() => {
 
 
 lobbyList.addEventListener("click", async(event) =>{
-    const room = event.target.closest("lobby-item") // closest = 상위부모 찾기 즉 해당 리스트 클릭하면 lobby-item 찾음
+    const room = event.target.closest(".lobby-item") // closest = 상위부모 찾기 즉 해당 리스트 클릭하면 lobby-item 찾음
     if(!room){
         alert("못 불러옴")
         console.error("못 불러옴")
+        return
     }
-
+    // js에서 data-로 시작하는 숨겨진 속성들만 뽑아오는 기능을 하는게 dataset임 그래서 위에 data-room-id로 함
     try{ // 일단 방 아이디 찾아와야 됨
-
+        const roomId = room.dataset.roomId
+        window.location.href = `room.html?roomId=${roomId}`
 
     }
     catch(error){
@@ -75,8 +74,4 @@ lobbyList.addEventListener("click", async(event) =>{
     }
 })
 
-
-
-// app.listen(4000, () => {
-//     console.log('서버 실행 중....')
-// })
+currentLobby()
